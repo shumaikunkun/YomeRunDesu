0e92e40dabaa1a0ed2ea218b843be7f849d565cc
shumaikunkun <s1711520@gmail.com>
2019-07-29 19:01:43 +0900

    GETメソッドからでもアクセス可能に

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'example#top'
   #get 'example/top'
+  get 'example/upload'
   post 'example/upload'
   post 'example/aaa'
   post 'example/bbb'
   post 'example/ccc'
+  get 'example/ddd'
   post 'example/ddd'
   get 'example/test'
 
