2136673170f378f7fcb4d89b1fb61fa03aea55b8
shumaikunkun <s1711520@gmail.com>
2019-12-05 04:43:16 +0900

    before_actionメソッドをハッシュを用いて一般化することに成功

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -2,16 +2,13 @@ class ExampleController < ApplicationController
   ["json","ibm_watson/visual_recognition_v3"].map{|lib|require lib}
   include IBMWatson
   protect_from_forgery
-  @@visual_recognition = VisualRecognitionV3.new( version: "2018-03-19", iam_apikey: ENV["APIKEY"] )
-
-  before_action->{ set_image(["food"],["ja"]) }, only: [:aaa]  #共通部分をまとめる
-  before_action->{ set_image(["default"],["ja"]) }, only: [:bbb]
-  before_action->{ set_image(["royal_162940933"],["en"]) }, only: [:ccc]
-
+  @@visual_recognition = VisualRecognitionV3.new(version: "2018-03-19", iam_apikey: ENV["APIKEY"])
+  #どの関数からでもアクセスできるクラス変数
+  {aaa:["food","ja"], bbb:["default","ja"], ccc:["royal_162940933","en"]}.map{|k,v|before_action->{set_image([v[0]],[v[1]])}, only:[k]}
+  #メソッドそれぞれに引数(分類機名と言語)を渡す（key:関数名, value:2つの引数）
   [:top, :upload, :aaa, :bbb, :ccc, :ddd, :test].map{|method|define_method(method){}}
-  #トップ画面、アップロード画面、食べ物認識、なんでも認識、顔認識、文字認識、テスト画面　それぞれのメソッドを一括定義
-
-  define_method(:set_image) do |classifier, lang|  #メソッドごとに分類器と言語が違うためそれを引数にする
+  #トップ画面、アップロード画面、食べ物認識、なんでも認識、顔認識、文字認識、テスト画面の順にそれぞれのメソッドを一括定義
+  define_method(:set_image) do |classifier, lang|  #メソッドごとに分類器と言語が違うためそれを関数の引数にする
     if request.post?
       @count=Count.find(1).count
       Count.update(count:@count+1)
