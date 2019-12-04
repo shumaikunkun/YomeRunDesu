980ffd5386f99d76f912e3ccf0bd6d59a2c13b0e
shumaikunkun <s1711520@gmail.com>
2019-12-04 16:55:12 +0900

    controllerの各メソッドの冗長な部分をset_imageメソッドにまとめて一般化する（それぞれに異なる引数を渡す部分は未実装）

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,36 +1,24 @@
 class ExampleController < ApplicationController
-
   require "json"
   require "ibm_watson/visual_recognition_v3"
   include IBMWatson
-
   protect_from_forgery
+  @@visual_recognition = VisualRecognitionV3.new( version: "2018-03-19", iam_apikey: ENV["APIKEY"] )
 
-  def top
-  end
+  before_action :set_image, only: [:aaa, :bbb, :ccc]  #共通部分をまとめる
 
-  def upload
-  end
-
-  @@visual_recognition = VisualRecognitionV3.new(
-    version: "2018-03-19",
-    iam_apikey: ENV["APIKEY"]
-  )
-
+  define_method(:top){}
+  define_method(:upload){}
   #文字認識
-  def ddd
-  end
+  define_method(:ddd){}
+
 
   #食べ物認識
   def aaa
     if request.post?
-      @count=Count.find(1).count
-      Count.update(count:@count+1)
-      File.binwrite("public/images/test#{@count}.jpg",params[:image].read)
-      @image="/images/test#{@count}.jpg"
       File.open("#{Rails.root}/public#{@image}") do |images_file|
         classes = @@visual_recognition.classify(images_file: images_file,classifier_ids:["food"],accept_language: ["ja"])
-        @aaa= JSON.parse(JSON.pretty_generate(classes.result))["images"][0]["classifiers"][0]["classes"]
+        @json= JSON.parse(JSON.pretty_generate(classes.result))["images"][0]["classifiers"][0]["classes"]
       end
     end
   end
@@ -38,13 +26,9 @@ class ExampleController < ApplicationController
   #ナンデモ認識
   def bbb
     if request.post?
-      @count=Count.find(1).count
-      Count.update(count:@count+1)
-      File.binwrite("public/images/test#{@count}.jpg",params[:image].read)
-      @image="/images/test#{@count}.jpg"
       File.open("#{Rails.root}/public#{@image}") do |images_file|
         classes = @@visual_recognition.classify(images_file: images_file,accept_language: ["ja"])
-        @bbb= JSON.parse(JSON.pretty_generate(classes.result))["images"][0]["classifiers"][0]["classes"]
+        @json= JSON.parse(JSON.pretty_generate(classes.result))["images"][0]["classifiers"][0]["classes"]
       end
     end
   end
@@ -52,20 +36,23 @@ class ExampleController < ApplicationController
   #顔認識
   def ccc
     if request.post?
-      @count=Count.find(1).count
-      Count.update(count:@count+1)
-      File.binwrite("public/images/test#{@count}.jpg",params[:image].read)
-      @image="/images/test#{@count}.jpg"
       File.open("#{Rails.root}/public#{@image}") do |images_file|
         # faces = @@visual_recognition.detect_faces(images_file: images_file)
         # @ccc=JSON.parse(JSON.pretty_generate(faces.result))["images"][0]["faces"][0]
         classes = @@visual_recognition.classify(images_file: images_file,classifier_ids:["royal_162940933"])
-        @json=JSON.parse(JSON.pretty_generate(classes.result))["images"][0]["classifiers"][0]["classes"]
-        @ccc= @json[0]
+        @json= JSON.parse(JSON.pretty_generate(classes.result))["images"][0]["classifiers"][0]["classes"]
       end
     end
   end
 
-  def test
-  end
+  define_method(:set_image){
+    if request.post?
+      @count=Count.find(1).count
+      Count.update(count:@count+1)
+      File.binwrite("public/images/test#{@count}.jpg",params[:image].read)
+      @image="/images/test#{@count}.jpg"
+    end
+  }
+
+  define_method(:test){}
 end
